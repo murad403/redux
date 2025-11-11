03aedc0c4b78213f645bf1194f6d9a31823f1248
murad403 <mdm78774@gmail.com>
2025-11-11 10:02:18 +0600

    refresh token logout

--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -1,36 +1,49 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import type { RootState } from "../store";
 import { setUser } from "../features/auth/authSlice";
+import axios from "axios";
 
+// base query-----------------------------------------------------------------------------------------------
 const baseQuery = fetchBaseQuery({
     baseUrl: "your base url",
     credentials: "include",
-    prepareHeaders: (headers, {getState}) =>{
+    prepareHeaders: (headers, { getState }) => {
         const token = (getState() as RootState).auth.accessToken;
-        if(token){
+        if (token) {
             headers.set('authorization', `Bearer ${token}`);
         }
         return headers;
     }
 })
 
-const baseQueryRefreshToken = async(args, api, extraoptions) =>{
-    const result = await baseQuery(args, api, extraoptions);
+
+// custome base query------------------------------------------------------------------------------- 
+const baseQueryRefreshToken = async (args, api, extraoptions) => {
+    let result = await baseQuery(args, api, extraoptions);
     console.log(result);
-    if(result.error?.status === 401){
+    if (result.error?.status === 401) {
         console.log("sending request for access token generate");
-        const data = req.json()//after sending request----
-        const user = (api.getState() as RootState).auth.user;
-        api.dispatch(setUser({user, accessToken: data?.accessToken}))
+        const res = await axios.post('/auth/resfresh-token-generate')
+        const data = await res.json()//after sending request----
+
+        if (data?.data.accessToken) {
+            const user = (api.getState() as RootState).auth.user;
+            api.dispatch(setUser({ user, accessToken: data?.accessToken }));
+            result = await baseQuery(args, api, extraoptions);
+        } else{
+            api.dispatch(logout());          //logout function from auth api------------------------------------------------
+        }
     }
     return result;
 }
+
+
 
 const baseApi = createApi({
     reducerPath: "baseApi",
     baseQuery: baseQueryRefreshToken,
     tagTypes: ["User"],
-    endpoints: () =>({
+    endpoints: () => ({
         // getUser: builder.query({
         //     query: (query) =>{
         //         const params = new URLSearchParams();
